0b481a02e510a837dce2dfe60120467e2c7b8e4c
Brian Carver <brarver@yahoo.com>
2019-07-01 18:42:24 -0600

    logic done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,33 @@
 ////////VARIABLES///////////////
 var words = ['mountain', 'avalanche', 'slope', 'summit', 'cornice']
 var lettersGuessed = []
-var wins = ''
+var wins = 0
 var guessesRemaining = 6
-var word 
+var word = []
+
 
 var wordDisplay = document.querySelector(".word-display")
 wordDisplay.textContent = 'Press any key to get started!'
 
+var guessedDisplay = document.querySelector(".letters-guessed-display")
+guessedDisplay.textContent =  `LettersGuessed: ${lettersGuessed}`
+var remainingDisplay = document.querySelector(".guesses-remaining-display")
+remainingDisplay.textContent = `Guesses Remaining: ${guessesRemaining}`
+var winsDisplay = document.querySelector(".wins-display")
+winsDisplay.textContent = `Wins: ${wins}`
+
+
+
+
 //////////FUNCTIONS//////////////
 
 var randomSplitWord = function (array) {
-    var word = array[Math.floor(Math.random() * array.length)]
-    return word.split('')
+    return array[Math.floor(Math.random() * array.length)].split('')
 }
 
 word = randomSplitWord(words)
 console.log(word)
+
 
 
 var displayWord = function (word, letters) {
@@ -41,28 +52,54 @@ var checkLetter = function (guess) {
     
     if (!lettersGuessed.includes(guess) && word.includes(guess)) {
         lettersGuessed.push(guess)
+        guessedDisplay.textContent = `Letters Guessed: ${lettersGuessed}`
         
     } else if (!lettersGuessed.includes(guess) && !word.includes(guess)) {
         guessesRemaining--
+        lettersGuessed.push(guess)
+        guessedDisplay.textContent = `Letters Guessed: ${lettersGuessed}`
+        remainingDisplay.textContent = `Guesses Remaining: ${guessesRemaining}`
     }
 }
 
-var checkWin = function (word, lettersGuessed) {
+var checkWin = function (arg) {
     
-    var winStatus = word.every(letter => {
+    var winStatus = arg.every(letter => {
          return lettersGuessed.includes(letter)
     })
+
 
     if (winStatus) {
         wordDisplay.textContent = 'You Win!'
         word = randomSplitWord(words)
-        console.log(word)
+        lettersGuessed = []
+        wins++
+        guessesRemaining = 6
+        winsDisplay.textContent = `Wins: ${wins}`
+        guessedDisplay.textContent =  `LettersGuessed: ${lettersGuessed}`
+        remainingDisplay.textContent = `Guesses Remaining: ${guessesRemaining}`
+        setTimeout(function () {
+            wordDisplay.innerHTML = ''
+            wordDisplay.appendChild(displayWord(word, lettersGuessed))
+        }, 2000)   
     }
 
     if (guessesRemaining === 0) {
         wordDisplay.textContent = 'You Lose!'
+        word = randomSplitWord(words)
+        lettersGuessed = []
+        guessesRemaining = 6
+        guessedDisplay.textContent =  `LettersGuessed: ${lettersGuessed}`
+        remainingDisplay.textContent = `Guesses Remaining: ${guessesRemaining}`
+        setTimeout(function () {
+            wordDisplay.innerHTML = ''
+            wordDisplay.appendChild(displayWord(word, lettersGuessed))
+        }, 2000)
     }
+
 }
+
+
 
 
 //////EVENTS/CALLS///////////////
@@ -73,9 +110,7 @@ document.addEventListener('keypress', function (e) {
     wordDisplay.appendChild(displayWord(word, lettersGuessed))
     checkWin(word, lettersGuessed) 
 })
-
-//install win/lose game reset!!!
-
+//yep
 
 
 
